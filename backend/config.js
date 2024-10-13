de6408bed439118c1f5fdd04a65e002189d935f2
AK-Developers <aryankhatri9811@gmail.com>
2024-10-13 20:57:29 +0530

    deployment

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,3 @@ export const PORT = 5000;
 
 export const mongoDBURL =
   'mongodb+srv://aryankhatri9811:aryan9811@cluster0.gcvtw5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
