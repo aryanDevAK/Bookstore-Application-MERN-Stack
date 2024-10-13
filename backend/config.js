d171d04a308ac7541d0f07d69dc97e6e63cdf1c4
AK-Developers <aryankhatri9811@gmail.com>
2024-10-13 21:12:08 +0530

    deployment

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,7 @@
-export const PORT = 5000;
+// export const PORT = 5000;
 
-export const mongoDBURL =
-  'mongodb+srv://aryankhatri9811:aryan9811@cluster0.gcvtw5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// export const mongoDBURL =
+//   'mongodb+srv://aryankhatri9811:aryan9811@cluster0.gcvtw5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+export const PORT = process.env.PORT || 5000;
+export const mongoDBURL = process.env.mongoDBURL;
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,17 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoDBURL = process.env.mongoDBURL;
 
-// Middleware for parsing request body
 app.use(express.json());
-
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
 
 app.get('/', (request, response) => {
   console.log(request);
